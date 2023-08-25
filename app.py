fc33183561dc324e97473f0d7d6403ae38f9b2f2
Russ <russellkohler8@gmail.com>
2023-08-25 15:31:58 -0500

    updated

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+from flask import Flask, request, render_template, jsonify, session
 from boggle import Boggle
 
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "abc123"
+
 boggle_game = Boggle()
+
+
+@app.route("/")
+def homepage():
+    """Show board."""
+
+    board = boggle_game.make_board()
+
+
